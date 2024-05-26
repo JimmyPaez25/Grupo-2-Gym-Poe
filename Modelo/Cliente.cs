8bcc13940ca97ada44d3b220e092c7c881f147fd
Andres Chavez <eliceojimenez1995@gmail.com>
2024-05-26 18:30:17 -0500

    Agregar VsRegistroCliente, clase Cliente y CtrlCliente

---
+++ Modelo/Cliente.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    internal class Cliente
+    {
+        int cedula;
+        String nombre;
+        String apellido;
+        DateTime fechaNacimiento;
+        int telefono;
+        String direccion;
+        char estudiante;
+
+        public int Cedula { get => cedula; set => cedula = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Apellido { get => apellido; set => apellido = value; }
+        public DateTime FechaNacimiento { get => fechaNacimiento; set => fechaNacimiento = value; }
+        public int Telefono { get => telefono; set => telefono = value; }
+        public string Direccion { get => direccion; set => direccion = value; }
+        public char Estudiante { get => estudiante; set => estudiante = value; }
+
+        public Cliente(int cedula, string nombre, string apellido, DateTime fechaNacimiento, int telefono, string direccion, char estudiante)
+        {
+            Cedula = cedula;
+            Nombre = nombre;
+            Apellido = apellido;
+            FechaNacimiento = fechaNacimiento;
+            Telefono = telefono;
+            Direccion = direccion;
+            Estudiante = estudiante;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+        
+    }
+}
---
+++ Vista/VsRegistroCliente.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace Vista
+{
+    partial class VsRegistroCliente
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblRegistro = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblCedula = new System.Windows.Forms.Label();
+            this.lblEstado = new System.Windows.Forms.Label();
+            this.lblEstudiante = new System.Windows.Forms.Label();
+            this.lblDireccion = new System.Windows.Forms.Label();
+            this.lblTelefono = new System.Windows.Forms.Label();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.lblApellido = new System.Windows.Forms.Label();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtCedula = new System.Windows.Forms.TextBox();
+            this.txtDate = new System.Windows.Forms.TextBox();
+            this.txtTelefono = new System.Windows.Forms.TextBox();
+            this.txtDireccion = new System.Windows.Forms.TextBox();
+            this.cmbEstudiante = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblRegistro
+            // 
+            this.lblRegistro.AutoSize = true;
+            this.lblRegistro.Font = new System.Drawing.Font("Microsoft YaHei", 20F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.lblRegistro.Location = new System.Drawing.Point(288, 9);
+            this.lblRegistro.Name = "lblRegistro";
+            this.lblRegistro.Size = new System.Drawing.Size(274, 36);
+            this.lblRegistro.TabIndex = 1;
+            this.lblRegistro.Text = "Registro de Cliente";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic);
+            this.lblName.Location = new System.Drawing.Point(82, 106);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(69, 20);
+            this.lblName.TabIndex = 4;
+            this.lblName.Text = "Nombre:";
+            // 
+            // lblCedula
+            // 
+            this.lblCedula.AutoSize = true;
+            this.lblCedula.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCedula.Location = new System.Drawing.Point(82, 82);
+            this.lblCedula.Name = "lblCedula";
+            this.lblCedula.Size = new System.Drawing.Size(63, 20);
+            this.lblCedula.TabIndex = 3;
+            this.lblCedula.Text = "Cedula:";
+            this.lblCedula.Click += new System.EventHandler(this.lblCedula_Click);
+            // 
+            // lblEstado
+            // 
+            this.lblEstado.AutoSize = true;
+            this.lblEstado.Location = new System.Drawing.Point(82, 250);
+            this.lblEstado.Name = "lblEstado";
+            this.lblEstado.Size = new System.Drawing.Size(0, 13);
+            this.lblEstado.TabIndex = 26;
+            // 
+            // lblEstudiante
+            // 
+            this.lblEstudiante.AutoSize = true;
+            this.lblEstudiante.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic);
+            this.lblEstudiante.Location = new System.Drawing.Point(82, 238);
+            this.lblEstudiante.Name = "lblEstudiante";
+            this.lblEstudiante.Size = new System.Drawing.Size(90, 20);
+            this.lblEstudiante.TabIndex = 25;
+            this.lblEstudiante.Text = "Estudiante:";
+            // 
+            // lblDireccion
+            // 
+            this.lblDireccion.AutoSize = true;
+            this.lblDireccion.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic);
+            this.lblDireccion.Location = new System.Drawing.Point(82, 212);
+            this.lblDireccion.Name = "lblDireccion";
+            this.lblDireccion.Size = new System.Drawing.Size(79, 20);
+            this.lblDireccion.TabIndex = 24;
+            this.lblDireccion.Text = "Dirección:";
+            this.lblDireccion.Click += new System.EventHandler(this.lblDireccion_Click);
+            // 
+            // lblTelefono
+            // 
+            this.lblTelefono.AutoSize = true;
+            this.lblTelefono.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic);
+            this.lblTelefono.Location = new System.Drawing.Point(82, 186);
+            this.lblTelefono.Name = "lblTelefono";
+            this.lblTelefono.Size = new System.Drawing.Size(75, 20);
+            this.lblTelefono.TabIndex = 23;
+            this.lblTelefono.Text = "Telefono:";
+            // 
+            // lblDate
+            // 
+            this.lblDate.AutoSize = true;
+            this.lblDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic);
+            this.lblDate.Location = new System.Drawing.Point(82, 160);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(163, 20);
+            this.lblDate.TabIndex = 22;
+            this.lblDate.Text = "Fecha de Nacimiento:";
+            // 
+            // lblApellido
+            // 
+            this.lblApellido.AutoSize = true;
+            this.lblApellido.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic);
+            this.lblApellido.Location = new System.Drawing.Point(82, 134);
+            this.lblApellido.Name = "lblApellido";
+            this.lblApellido.Size = new System.Drawing.Size(69, 20);
+            this.lblApellido.TabIndex = 21;
+            this.lblApellido.Text = "Apellido:";
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Location = new System.Drawing.Point(256, 131);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(202, 20);
+            this.txtApellido.TabIndex = 29;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(256, 103);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(202, 20);
+            this.txtNombre.TabIndex = 28;
+            // 
+            // txtCedula
+            // 
+            this.txtCedula.Location = new System.Drawing.Point(256, 79);
+            this.txtCedula.Name = "txtCedula";
+            this.txtCedula.Size = new System.Drawing.Size(202, 20);
+            this.txtCedula.TabIndex = 27;
+            // 
+            // txtDate
+            // 
+            this.txtDate.Location = new System.Drawing.Point(256, 157);
+            this.txtDate.Name = "txtDate";
+            this.txtDate.Size = new System.Drawing.Size(202, 20);
+            this.txtDate.TabIndex = 33;
+            // 
+            // txtTelefono
+            // 
+            this.txtTelefono.Location = new System.Drawing.Point(256, 183);
+            this.txtTelefono.Name = "txtTelefono";
+            this.txtTelefono.Size = new System.Drawing.Size(202, 20);
+            this.txtTelefono.TabIndex = 32;
+            // 
+            // txtDireccion
+            // 
+            this.txtDireccion.Location = new System.Drawing.Point(256, 209);
+            this.txtDireccion.Name = "txtDireccion";
+            this.txtDireccion.Size = new System.Drawing.Size(202, 20);
+            this.txtDireccion.TabIndex = 31;
+            // 
+            // cmbEstudiante
+            // 
+            this.cmbEstudiante.FormattingEnabled = true;
+            this.cmbEstudiante.Location = new System.Drawing.Point(256, 235);
+            this.cmbEstudiante.Name = "cmbEstudiante";
+            this.cmbEstudiante.Size = new System.Drawing.Size(84, 21);
+            this.cmbEstudiante.TabIndex = 30;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(228, 316);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 34;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // VsRegistroCliente
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(825, 571);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtDate);
+            this.Controls.Add(this.txtTelefono);
+            this.Controls.Add(this.txtDireccion);
+            this.Controls.Add(this.cmbEstudiante);
+            this.Controls.Add(this.txtApellido);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.txtCedula);
+            this.Controls.Add(this.lblEstado);
+            this.Controls.Add(this.lblEstudiante);
+            this.Controls.Add(this.lblDireccion);
+            this.Controls.Add(this.lblTelefono);
+            this.Controls.Add(this.lblDate);
+            this.Controls.Add(this.lblApellido);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.lblCedula);
+            this.Controls.Add(this.lblRegistro);
+            this.Name = "VsRegistroCliente";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblRegistro;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblCedula;
+        private System.Windows.Forms.Label lblEstado;
+        private System.Windows.Forms.Label lblEstudiante;
+        private System.Windows.Forms.Label lblDireccion;
+        private System.Windows.Forms.Label lblTelefono;
+        private System.Windows.Forms.Label lblDate;
+        private System.Windows.Forms.Label lblApellido;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtCedula;
+        private System.Windows.Forms.TextBox txtDate;
+        private System.Windows.Forms.TextBox txtTelefono;
+        private System.Windows.Forms.TextBox txtDireccion;
+        private System.Windows.Forms.ComboBox cmbEstudiante;
+        private System.Windows.Forms.Button button1;
+    }
+}
