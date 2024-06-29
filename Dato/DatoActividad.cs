42be68d850c95fb1f6cff66232946c164290a044
“ADRIAN <adrian.gonzalezjaramillo@gmail.com>
2024-06-29 01:15:41 -0500

    INSERT 2

--- Dato/DatoActividad.cs
+++ Dato/DatoActividad.cs
@@ -8,11 +8,40 @@ using Modelo;
 
 namespace Dato
 {
+    // GONZALEZ ASTUDILLO ADRIAN
     public class DatoActividad
     {
-        public string IngresarActividad(Actividad act, SqlConnection cn)
+        SqlCommand cmd = new SqlCommand();
+
+        public string IngresarActividad(Actividad act, SqlConnection conn)
         {
-            throw new NotImplementedException();
+            Console.WriteLine("-----INSERT ACTIVIDAD-----");
+            string resultado;
+            string comando = "INSERT INTO Actividad(Estado, Nombre, Descripcion, FechaInicio, FechaFin, HoraInicio, HoraFin) \n" +
+                             "VALUES (@Estado, @Nombre, @Descripcion, @FechaInicio, @FechaFin, @HoraInicio, @HoraFin); \n";
+
+            try
+            {
+                cmd.Connection = conn;
+                cmd.CommandText = comando;
+
+                cmd.Parameters.AddWithValue("@Estado", act.Estado);
+                cmd.Parameters.AddWithValue("@Nombre", act.Nombre);
+                cmd.Parameters.AddWithValue("@Descripcion", act.Descripcion);
+                cmd.Parameters.AddWithValue("@FechaInicio", act.FechaInicio);
+                cmd.Parameters.AddWithValue("@FechaFin", act.FechaFin);
+                cmd.Parameters.AddWithValue("@HoraInicio", act.HoraInicio);
+                cmd.Parameters.AddWithValue("@HoraFin", act.HoraFin);
+
+                cmd.ExecuteNonQuery();
+                resultado = "1";
+            }
+            catch (Exception ex)
+            {
+                resultado = "0" + ex.Message;
+                Console.WriteLine(resultado);
+            }
+            return resultado;
         }
 
 
